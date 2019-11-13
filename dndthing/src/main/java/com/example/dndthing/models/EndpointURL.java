8c962771b39092f046dbfd0994d267558914cdf8
sidewayscow <siva.yedithi@gmail.com>
2019-11-13 11:58:53 -0500

    Added class for restTemplate to marshall response to

---
+++ dndthing/src/main/java/com/example/dndthing/models/EndpointURL.java
@@ -0,0 +1,22 @@
+package com.example.dndthing.models;
+
+import java.util.List;
+
+public class EndpointURL {
+
+    private int count;
+    private List<Result> results;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+}
